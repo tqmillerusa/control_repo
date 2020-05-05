a5dccaa8d5c501682c147c17fbf5df3e93e006ef
tqmillerusa <46930787+tqmillerusa@users.noreply.github.com>
2020-05-05 17:15:37 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -2,11 +2,11 @@ class profile::ssh_server {
   package {'openssh-server':
     ensure => present,
   }
-  service { 'sshd':
+  service {'sshd':
     ensure => 'running',
     enable => 'true',
   }
-  ssh_authorized_keys {'root@master.puppet.vm':
+  ssh_authorized_key { 'root@master.puppet.vm':
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
